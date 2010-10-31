21e54f01f922038b9d20133958f85a39c7100cec
Karl Jonathan Ward <karl.j.ward@gmail.com>
2010-10-31 23:49:57 +0000

    DepositSplitter.groovy: Working deposit splitter and updates to DepLoader driver.

--- groovy/src/DepLoader.groovy
+++ groovy/src/DepLoader.groovy
@@ -1,26 +1,39 @@
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import groovy.sql.Sql
 import javax.xml.parsers.SAXParserFactory
 import org.xml.sax.helpers.DefaultHandler
 import org.xml.sax.*
 
-def sql = Sql.newInstance("jdbc:mysq://localhost/deploader", "root", "root")
+def sql = Sql.newInstance("jdbc:mysql://localhost/deploader", "root", "root")
 def inDir = "/Users/karl/Proj/deploader/in"
 def outDir = "/Users/karl/Proj/deploader/out"
 
-new File(inDir).forEach {
+def writeXml(doc) {
+    def source = new DOMSource(doc)
+    def bout = new ByteArrayOutputStream()
+    def result = new StreamResult(bout)
+    def xformer = TransformerFactory.newInstance().newTransformer()
+    xformer.transform(source, result)
+    return bout.toByteArray()
+}
+
+new File(inDir).eachFile {
     // Do an insert for each deposit in the XML.
     def splitter = new DepositSplitter()
-    def reader = SAXParserFacory.newInstace().newSAXParser().XMLReader
+    splitter.parseFile(it)
     
-    reader.setContentHandler(splitter)
-    reader.parse(new InputSource(new FileInputStream(it)))
-    
-    splitter.forEach { doi, xml ->
-        sql.execute("INSERT INTO deposits (doi, xml) VALUES (${doi}, ${xml})")
+    splitter.each { doc ->
+        def doi = doc.getElementsByTagName("doi").item(0).getFirstChild().getData()
+        
+        def bytes = writeXml(doc)
+        sql.execute("INSERT INTO deposits (doi, xml) VALUES (${doi}, ${bytes})")
     }
     
     // Move file into the 'out' directory.
-    it.renameTo(outDir + "/" + it.getName())
+    //it.renameTo(outDir + "/" + it.getName())
 }
 
 
--- groovy/src/DepositSplitter.groovy
+++ groovy/src/DepositSplitter.groovy
@@ -1,7 +1,131 @@
-import javax.xml.parsers.SAXParserFactory
-import org.xml.sax.helpers.DefaultHandler
-import org.xml.sax.*
+import java.io.File;
 
-class DepositSplitter extends DefaultHandler {
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.stream.EventFilter;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.events.XMLEvent;
+
+import org.codehaus.groovy.runtime.IteratorClosureAdapter;
+
+import com.sun.tools.internal.ws.wsdl.document.Documentation;
+
+class DepositSplitter implements Iterable, Iterator {
+    
+    private containerDoc
+    
+    private containerRoot
+    
+    private eleReader
+    
+    private reader
+    
+    private lastDoiRecordAppend
+    
+    def parseFile(File file) {
+        def factory = XMLInputFactory.newInstance()
+        reader = factory.createXMLEventReader(new FileInputStream(file))
+        
+        eleReader = factory.createFilteredReader(reader,
+            new EventFilter() {
+                public boolean accept(XMLEvent event) {
+                    if (event.isStartElement()) {
+                        return ("doi_record".equals(event.asStartElement()
+                                                        .getName()
+                                                        .getLocalPart())
+                             || "publisher".equals(event.asStartElement()
+                                                        .getName()
+                                                        .getLocalPart())
+                             || "publication".equals(event.asStartElement()
+                                                          .getName()
+                                                          .getLocalPart()))
+                    }
+                    return false
+                }
+            }
+        )
+        
+        def docFact = DocumentBuilderFactory.newInstance()
+        def builder = docFact.newDocumentBuilder()
+        def impl = builder.getDOMImplementation()
+        
+        containerDoc = impl.createDocument(null, null, null)
+        
+        while (eleReader.hasNext()) {
+            XMLEvent e = eleReader.next()
+            
+            def startEleEvent = e.asStartElement()
+            def name = startEleEvent.getName().getLocalPart()
+            
+            if ("publication".equals(name)) {
+                containerRoot = readElement(startEleEvent)
+                containerDoc.appendChild(containerRoot)
+            } else if ("publisher".equals(name)) {
+                containerRoot.appendChild(readPartialTree(startEleEvent))
+                break
+                // Now we read doi_record elements via this class's 
+                // iterator.
+            }
+        }
+    }
+    
+    def readElement(e) {
+        def ele = containerDoc.createElement(e.getName().getLocalPart())
+        e.getAttributes().each {
+            ele.setAttribute(it.getName().getLocalPart(),
+                             it.getValue())
+        }
+        return ele
+    }
+    
+    def readPartialTree(e) {
+        def child = readElement(e)
+        
+        while (reader.hasNext()) {
+            XMLEvent event = reader.next()
+            if (event.isCharacters()) {
+                def text = containerDoc.createTextNode()
+                text.setData(event.asCharacters().data)
+                child.appendChild(text)
+            } else if (event.isStartElement()) {
+                def ele = readPartialTree(event)
+                child.appendChild(ele)
+            } else if (event.isEndElement()) {
+                break
+            }
+        }
+        
+        return child
+    }
+    
+    boolean hasNext() {
+        return eleReader.hasNext()
+    }
+    
+    Object next() {
+        if (lastDoiRecordAppend != null) {
+            containerRoot.removeChild(lastDoiRecordAppend)
+            lastDoiRecordAppend = null
+        }
+        
+        XMLEvent e = eleReader.next()
+        
+        def startEleEvent = e.asStartElement()
+        def name = startEleEvent.getName().getLocalPart()
+        
+        if ("doi_record".equals(name)) {
+            lastDoiRecordAppend = readPartialTree(e)
+            containerRoot.appendChild(lastDoiRecordAppend)
+        }
+        
+        return containerDoc
+    }
+    
+    void remove() {
+        throw new UnsupportedOperationException()
+    }
+    
+    Iterator<Map> iterator() {
+        return this
+	}
     
 }
